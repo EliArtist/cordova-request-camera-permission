1acc11961c853e2e1594ec40f0705616edd6de17
EliArtist <elias-kuenstler@web.de>
2017-09-05 22:01:36 +0200

    initial push, hopefully working already #5

--- src/android/CameraPermission.java
+++ src/android/CameraPermission.java
@@ -12,15 +12,17 @@ public class CameraPermission extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("init")) {
-            if(cordova.hasPermissions(Manifest.permission.CAMERA))
+            if(cordova.hasPermissions(Manifest.permission.CAMERA)) {
                 return true;
-            else
+            } else {
                 cordova.requestPermissions(this, 0, [Manifest.permission.CAMERA]);
+            }
 
-            if(cordova.hasPermissions(Manifest.permission.CAMERA))
+            if(cordova.hasPermissions(Manifest.permission.CAMERA)) {
                 return true;
-            else
+            } else {
                 return false;
+            }
         }
         return false;
     }
