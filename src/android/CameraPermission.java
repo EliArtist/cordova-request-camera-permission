0bc41a70d2ebbd5b00cccc70930a223539d9feb4
EliArtist <elias-kuenstler@web.de>
2017-09-05 22:16:01 +0200

    initial push, hopefully working already #8

--- src/android/CameraPermission.java
+++ src/android/CameraPermission.java
@@ -4,6 +4,7 @@ import android.Manifest;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 
 import org.json.JSONArray;
 import org.json.JSONException;
