683706390745952a435a1a7556c159f0383a8be1
EliArtist <elias-kuenstler@web.de>
2017-09-05 22:34:35 +0200

    initial push, hopefully working already #11

--- www/ContextMenu.js
+++ www/ContextMenu.js
@@ -4,4 +4,11 @@ CameraPermission.prototype.init = function(onSuccess, onError) {
     exec(onSuccess, onError, "CameraPermission", "init");
 }
 
-module.exports = CameraPermission;
+cordova.addConstructor(function() {
+    if(!window.Cordova) {
+        window.Cordova = cordova;
+    }
+
+    if(!window.plugins) window.plugins = {};
+    window.plugins.CameraPermission = new CameraPermission();
+});
